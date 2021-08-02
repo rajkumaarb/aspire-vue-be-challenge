8ddabb7395276696d83294b92e96e3b81cac2103
Rajkumaar <Bharath.Rajkumaar@agilysys.com>
2021-08-02 17:10:53 +0530

    deployment cycles

--- app/index.js
+++ app/index.js
@@ -24,24 +24,16 @@ app.use((req, res, next) => {
 	next();
 });
 app.use(express.json());
-// app.use(express.static(path.resolve(__dirname, "public")));
 
 /* APIs */
 app.use("/api/user", userAPIs);
 app.use("/api/loan", loanAPIs);
 
-//TODO:: Not to be removed
-// if (process.env.NODE_ENV === 'production') {
-// app.use(express.static('public'));
 
-// app.get("*", (req, res) => {
-// 	res.sendFile(path.resolve(__dirname, "public", "index.html"));
-// });
-// }
-// app.use(function (req, res, next) {
-// 	res.status(404).json({ errorMessage: "Sorry can't find that!" });
-// });
-
+app.use(express.static(path.resolve(__dirname, "..", "client-app")));
+app.get("*", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "..", "client-app", "index.html"));
+});
 app.use(function (err, req, res, next) {
 	res.status(500).send({ errorMessage: "INTERNAL_SERVER_ERROR" });
 });
